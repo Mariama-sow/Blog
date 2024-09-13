355701fd786310b44f21c2d4eb349a55f9d496cf
Mariama Sow <sowmariama2385@gmail.com>
2024-09-13 16:26:19 +0200

    ajout de la base de donné mysql

--- Articles/admin.py
+++ Articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Articles , Comment
 
-# Register your models here.
+class Articlesadmin(admin.ModelAdmin):
+    list_display=('title','sumary','date_pub','created_at')
+    date_hierarchy = 'date_pub'
+
+admin.site.register(Articles,Articlesadmin)
--- blog/settings.py
+++ blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,8 +23,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v!o@vhlowir%#v-2^$k76kl%gas6z0k#b+^ti+gby@9+7g81n5'
-
+#SECRET_KEY = 'django-insecure-v!o@vhlowir%#v-2^$k76kl%gas6z0k#b+^ti+gby@9+7g81n5'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -78,14 +79,23 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
